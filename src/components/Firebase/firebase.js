58ed40886080a548489d280cbe93c17c5fdb7cdc
alessandroiori <alessandro.iori92@gmail.com>
2019-09-09 16:48:16 +0200

    added firebase sign up

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const prodConfig = {
     apiKey: process.env.REACT_APP_PROD_API_KEY,
@@ -16,11 +17,28 @@ const devConfig = {
     storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
 };
+
 const config =
     process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
 class Firebase {
     constructor() {
         app.initializeApp(config);
+        this.auth = app.auth();
     }
+
+    // *** Auth API ***
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password =>
+        this.auth.currentUser.updatePassword(password);
 }
 export default Firebase;
