1aea67d7b0c831bc9e8de86844505f697d950887
alessandroiori <alessandro.iori92@gmail.com>
2019-09-10 19:05:29 +0200

    added browser session persistence

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -7,20 +7,25 @@ const withAuthentication = Component => {
     class WithAuthentication extends React.Component {
         constructor(props) {
             super(props);
+
             this.state = {
-                authUser: null,
+                authUser: JSON.parse(localStorage.getItem('authUser')),
             };
         }
+
         componentDidMount() {
             this.listener = this.props.firebase.onAuthUserListener(
                 authUser => {
+                    localStorage.setItem('authUser', JSON.stringify(authUser));
                     this.setState({ authUser });
                 },
                 () => {
+                    localStorage.removeItem('authUser');
                     this.setState({ authUser: null });
                 },
             );
         }
+
         componentWillUnmount() {
             this.listener();
         }
