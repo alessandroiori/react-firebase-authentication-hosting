7e7035c230fb813384497e6680285df29eaa081f
alessandroiori <alessandro.iori92@gmail.com>
2019-09-17 16:57:24 +0200

    added Google Log In

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -27,6 +27,7 @@ class Firebase {
         app.initializeApp(config);
         this.auth = app.auth();
         this.db = app.database();
+        this.googleProvider = new app.auth.GoogleAuthProvider();
     }
 
     // *** Auth API ***
@@ -35,6 +36,9 @@ class Firebase {
 
     doSignInWithEmailAndPassword = (email, password) =>
         this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignInWithGoogle = () =>
+        this.auth.signInWithPopup(this.googleProvider);
 
     doSignOut = () => this.auth.signOut();
 
